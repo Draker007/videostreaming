86bbe68546d6be2d505772324b1397040cc1b46b
Draker <drakerdeepak@gmail.com>
2019-01-18 15:36:22 +0530

    Initial

--- app/src/main/java/paramhans/videostreaming/MainActivity.java
+++ app/src/main/java/paramhans/videostreaming/MainActivity.java
@@ -1,8 +1,11 @@
 package paramhans.videostreaming;
 
+import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.VideoView;
 
@@ -12,7 +15,8 @@ public class MainActivity extends AppCompatActivity {
 
     private VideoView mainVideo ;
     private ProgressBar buffer;
-    private boolean isPlaying;
+    private boolean isPlaying = false;
+    private int currentState;
     private Uri videoUri = Uri.parse("https://firebasestorage.googleapis.com/v0/b/videostreaming-25f3a.appspot.com/o/happier.mp4?alt=media&token=e4475da6-31ed-4016-bb3e-02d78a951732");
 
 
@@ -27,5 +31,24 @@ public class MainActivity extends AppCompatActivity {
         mainVideo.setVideoURI(videoUri);
         mainVideo.requestFocus();
         mainVideo.start();
+        //buffer.setVisibility(View.INVISIBLE);
+
+
+        mainVideo.setOnInfoListener(new MediaPlayer.OnInfoListener() {
+            @Override
+            public boolean onInfo(MediaPlayer mp, int what, int extra) {
+                if( what == mp.MEDIA_INFO_BUFFERING_START){
+                    buffer.setVisibility(View.VISIBLE);
+
+                }
+                else if (what == mp.MEDIA_INFO_BUFFERING_END)
+                {
+                    buffer.setVisibility(View.INVISIBLE);
+                }
+                return false;
+            }
+        });
     }
+
+
 }
